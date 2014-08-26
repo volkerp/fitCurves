ff6be89ca2b841d5c27892631b895f8c36f8d172
Volker Poplawski <volker@poplawski.de>
2014-08-26 23:55:02 +0200

    initial commit

---
+++ demo.py
@@ -0,0 +1,115 @@
+""" Tkinter demo gui for bezier fitting algorithm
+
+     (c) Volker Poplawski 2014
+"""
+from __future__ import print_function
+from numpy import array
+from bezier import *
+from fitCurves import *
+from Tkinter import *
+
+
+# center of bounding box
+def cntr(x1, y1, x2, y2):
+    return x1+(x2-x1)/2, y1+(y2-y1)/2
+
+
+# tkinter Canvas plus some addons
+class MyCanvas(Canvas):
+    def create_polyline(self, points, **kwargs):
+        for p1, p2 in zip(points, points[1:]):
+            self.create_line(p1, p2, kwargs)
+
+    def create_bezier(self, b):
+        self.create_polyline([bezier.q(b, t/100.0).tolist() for t in xrange(0, 101)], tag='bezier', fill='blue', width='2') # there a better ways to draw a bezier
+        self.create_line(b[0].tolist(), b[1].tolist(), tags='bezier')
+        self.createPoint(b[1][0], b[1][1], 2, fill='black', tags='bezier')
+        self.create_line(b[3].tolist(), b[2].tolist(), tags='bezier')
+        self.createPoint(b[2][0], b[2][1], 2, fill='black', tags='bezier')
+
+    def createPoint(self, x, y, r, **kwargs):
+        return self.create_oval(x-r, y-r, x+r, y+r, kwargs)
+
+    def pos(self, idOrTag):
+        return cntr(*self.coords(idOrTag))
+
+    def itemsAtPos(self, x, y, tag):
+        return [item for item in self.find_overlapping(x, y, x, y) if tag in self.gettags(item)]
+
+
+class MainObject:
+    def run(self):
+        root = Tk()
+
+        self.canvas = MyCanvas(root, bg='white', width=400, height=400)
+        self.canvas.pack(side=LEFT)
+
+        frame = Frame(root, relief=SUNKEN, borderwidth=1)
+        frame.pack(side=LEFT, fill=Y)
+        label = Label(frame, text='Max Error')
+        label.pack()
+        self.spinbox = Spinbox(frame, width=8, from_=0.0, to=1000000.0, command=self.onSpinBoxValueChange)
+        self.spinbox.insert(0, 10.0)
+        self.spinbox.pack()
+
+        self.points = []
+        self.draggingPoint = None
+
+        self.canvas.bind('<ButtonPress-1>', self.onButton1Press)
+        self.canvas.bind('<ButtonPress-2>', self.onButton2Press)
+        self.canvas.bind('<B1-Motion>', self.onMouseMove)
+        self.canvas.bind('<ButtonRelease-1>', self.onButton1Release)
+
+        root.mainloop()
+
+
+    def onButton1Press(self, event):
+        items = self.canvas.itemsAtPos(event.x, event.y, 'point')
+        if items:
+            self.draggingPoint = items[0]
+        else:
+            self.points.append(self.canvas.createPoint(event.x, event.y, 4, fill='red', tag='point'))
+            self.redraw()
+
+
+    def onButton2Press(self, event):
+        self.canvas.delete(self.points.pop())
+        self.redraw()
+
+
+    def onMouseMove(self, event):
+        if self.draggingPoint:
+            self.canvas.coords(self.draggingPoint, event.x-4, event.y-4, event.x+4, event.y+4)
+            self.redraw()
+
+
+    def onButton1Release(self, event):
+        self.draggingPoint = None
+
+
+    def onSpinBoxValueChange(self):
+        self.redraw()
+
+
+    def redraw(self):
+        # redraw polyline
+        self.canvas.delete('polyline')
+        self.canvas.create_polyline([self.canvas.pos(pId) for pId in self.points], fill='grey', tag='polyline')
+        self.canvas.tag_lower('polyline')
+
+        # redraw bezier
+        if len(self.points) < 2:
+            return
+
+        self.canvas.delete('bezier')
+        points = array([self.canvas.pos(p) for p in self.points])
+        beziers = fitCurve(points, float(self.spinbox.get())**2)
+        for bezier in beziers:
+            self.canvas.create_bezier(bezier)
+
+
+if __name__ == '__main__':
+    o = MainObject()
+    o.run()
+
+
---
+++ fitCurves.py
@@ -0,0 +1,161 @@
+""" Python implementation of
+    Algorithm for Automatically Fitting Digitized Curves
+    by Philip J. Schneider
+    "Graphics Gems", Academic Press, 1990
+"""
+from __future__ import print_function
+from numpy import *
+import bezier
+
+
+# Fit one (ore more) Bezier curves to a set of points
+def fitCurve(points, maxError):
+    leftTangent = normalize(points[1] - points[0])
+    rightTangent = normalize(points[-2] - points[-1])
+    return fitCubic(points, leftTangent, rightTangent, maxError)
+
+
+def fitCubic(points, leftTangent, rightTangent, error):
+    # Use heuristic if region only has two points in it
+    if (len(points) == 2):
+        dist = linalg.norm(points[0] - points[1]) / 3.0
+        bezCurve = [points[0], points[0] + leftTangent * dist, points[1] + rightTangent * dist, points[1]]
+        return [bezCurve]
+
+    # Parameterize points, and attempt to fit curve
+    u = chordLengthParameterize(points)
+    bezCurve = generateBezier(points, u, leftTangent, rightTangent)
+    # Find max deviation of points to fitted curve
+    maxError, splitPoint = computeMaxError(points, bezCurve, u)
+    if maxError < error:
+        return [bezCurve]
+
+    # If error not too large, try some reparameterization and iteration
+    if maxError < error**2:
+        for i in range(20):
+            uPrime = reparameterize(bezCurve, points, u)
+            bezCurve = generateBezier(points, uPrime, leftTangent, rightTangent)
+            maxError, splitPoint = computeMaxError(points, bezCurve, uPrime)
+            if maxError < error:
+                return [bezCurve]
+            u = uPrime
+
+    # Fitting failed -- split at max error point and fit recursively
+    beziers = []
+    centerTangent = normalize(points[splitPoint-1] - points[splitPoint+1])
+    beziers += fitCubic(points[:splitPoint+1], leftTangent, centerTangent, error)
+    beziers += fitCubic(points[splitPoint:], -centerTangent, rightTangent, error)
+
+    return beziers
+
+
+def generateBezier(points, parameters, leftTangent, rightTangent):
+    bezCurve = [points[0], None, None, points[-1]]
+
+    # compute the A's
+    A = zeros((len(parameters), 2, 2))
+    for i, u in enumerate(parameters):
+        A[i][0] = leftTangent  * 3*(1-u)**2 * u
+        A[i][1] = rightTangent * 3*(1-u)    * u**2
+
+    # Create the C and X matrices
+    C = zeros((2, 2))
+    X = zeros(2)
+
+    for i, (point, u) in enumerate(zip(points, parameters)):
+        C[0][0] += dot(A[i][0], A[i][0])
+        C[0][1] += dot(A[i][0], A[i][1])
+        C[1][0] += dot(A[i][0], A[i][1])
+        C[1][1] += dot(A[i][1], A[i][1])
+
+        tmp = point - bezier.q([points[0], points[0], points[-1], points[-1]], u)
+
+        X[0] += dot(A[i][0], tmp)
+        X[1] += dot(A[i][1], tmp)
+
+    # Compute the determinants of C and X
+    det_C0_C1 = C[0][0] * C[1][1] - C[1][0] * C[0][1]
+    det_C0_X  = C[0][0] * X[1] - C[1][0] * X[0]
+    det_X_C1  = X[0] * C[1][1] - X[1] * C[0][1]
+
+    # Finally, derive alpha values
+    alpha_l = 0.0 if det_C0_C1 == 0 else det_X_C1 / det_C0_C1
+    alpha_r = 0.0 if det_C0_C1 == 0 else det_C0_X / det_C0_C1
+
+    # If alpha negative, use the Wu/Barsky heuristic (see text) */
+    # (if alpha is 0, you get coincident control points that lead to
+    # divide by zero in any subsequent NewtonRaphsonRootFind() call. */
+    segLength = linalg.norm(points[0] - points[-1])
+    epsilon = 1.0e-6 * segLength
+    if alpha_l < epsilon or alpha_r < epsilon:
+        # fall back on standard (probably inaccurate) formula, and subdivide further if needed.
+        bezCurve[1] = bezCurve[0] + leftTangent * (segLength / 3.0)
+        bezCurve[2] = bezCurve[3] + rightTangent * (segLength / 3.0)
+
+    else:
+        # First and last control points of the Bezier curve are
+        # positioned exactly at the first and last data points
+        # Control points 1 and 2 are positioned an alpha distance out
+        # on the tangent vectors, left and right, respectively
+        bezCurve[1] = bezCurve[0] + leftTangent * alpha_l
+        bezCurve[2] = bezCurve[3] + rightTangent * alpha_r
+
+    return bezCurve
+
+
+def reparameterize(bezier, points, parameters):
+    return [newtonRaphsonRootFind(bezier, point, u) for point, u in zip(points, parameters)]
+
+
+def newtonRaphsonRootFind(bez, point, u):
+    """
+       Newton's root finding algorithm calculates f(x)=0 by reiterating
+       x_n+1 = x_n - f(x_n)/f'(x_n)
+
+       We are trying to find curve parameter u for some point p that minimizes
+       the distance from that point to the curve. Distance point to curve is d=q(u)-p.
+       At minimum distance the point is perpendicular to the curve.
+       We are solving
+       f = q(u)-p * q'(u) = 0
+       with
+       f' = q'(u) * q'(u) + q(u)-p * q''(u)
+
+       gives
+       u_n+1 = u_n - |q(u_n)-p * q'(u_n)| / |q'(u_n)**2 + q(u_n)-p * q''(u_n)|
+    """
+    d = bezier.q(bez, u)-point
+    numerator = (d * bezier.qprime(bez, u)).sum()
+    denominator = (bezier.qprime(bez, u)**2 + d * bezier.qprimeprime(bez, u)).sum()
+
+    if denominator == 0.0:
+        return u
+    else:
+        return u - numerator/denominator
+
+
+def chordLengthParameterize(points):
+    u = [0.0]
+    for i in range(1, len(points)):
+        u.append(u[i-1] + linalg.norm(points[i] - points[i-1]))
+
+    for i, _ in enumerate(u):
+        u[i] = u[i] / u[-1]
+
+    return u
+
+
+def computeMaxError(points, bez, parameters):
+    maxDist = 0.0
+    splitPoint = len(points)/2
+    for i, (point, u) in enumerate(zip(points, parameters)):
+        dist = linalg.norm(bezier.q(bez, u)-point)**2
+        if dist > maxDist:
+            maxDist = dist
+            splitPoint = i
+
+    return maxDist, splitPoint
+
+
+def normalize(v):
+    return v / linalg.norm(v)
+
